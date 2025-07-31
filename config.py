0371e9f8bcae3c807a282feb441330e272a0044e
mimili7 <rukru4608@gmail.com>
2025-08-01 02:33:21 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv "7643439188:AAGybx-Ou2LfqYlfXjUqyZojqEpwgqMRCVU"
 CRYPTOMUS_API_KEY = os.getenv("CRYPTOMUS_API_KEY")
 CRYPTOMUS_SHOP_ID = os.getenv("CRYPTOMUS_SHOP_ID")
 BASE_URL = os.getenv("BASE_URL")
