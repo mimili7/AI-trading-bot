8d1f76b262eca1954dadded87d13cc8cb6cadc60
mimili7 <rukru4608@gmail.com>
2025-08-01 02:18:42 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CRYPTOMUS_API_KEY = os.getenv("CRYPTOMUS_API_KEY")
+CRYPTOMUS_SHOP_ID = os.getenv("CRYPTOMUS_SHOP_ID")
+BASE_URL = os.getenv("BASE_URL")
